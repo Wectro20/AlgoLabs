b9425f6c9ae6da174ad3d040237892f43f4cb9ce
Wectro20 <74012654+Wectro20@users.noreply.github.com>
2021-09-23 13:26:45 +0300

    Adding code to lab2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> a = new PriorityQueue<>();
+        a.add(21,20);
+        a.add(22,1);
+        a.add(22,0);
+        a.add(33,6);
+        a.add(6,100);
+        a.add(22,5);
+        a.print();
+
+    }
+}
+
+
+
