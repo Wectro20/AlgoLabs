594b5c1b1bf2aee9903f82ee44ef9bf4fa8bfe27
Wectro20 <74012654+Wectro20@users.noreply.github.com>
2021-10-04 22:25:47 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Bananas banana = new Bananas(new int[] {30,11,23,4,20}, 5);
+        Bananas banana = new Bananas(new int[] {3,6,7,11}, 8);
         banana.comparsion();
+        Bananas banana1 = new Bananas(new int[] {30,11,23,4,20}, 5);
+        banana1.comparsion();
+        Bananas banana2 = new Bananas(new int[] {30,11,23,4,20}, 6 );
+        banana2.comparsion();
     }
 }
